7a80a873440b8141ab041b9350882f6569bd3f98
Serhiy Rosovskyy <serhiyrosovskyy@Serhiys-MacBook-Air.local>
2019-01-31 16:19:21 +0200

    Getting json data

---
+++ SunsetApp/Model/SunSet.swift
@@ -0,0 +1,15 @@
+//
+//  SunSet.swift
+//  SunsetApp
+//
+//  Created by Serhiy Rosovskyy on 1/31/19.
+//  Copyright © 2019 Serhiy Rosovskyy. All rights reserved.
+//
+
+import Foundation
+
+
+enum SunSet {
+    case sunset
+    case sunrise
+}
--- SunsetApp/ViewController.swift
+++ SunsetApp/ViewController.swift
@@ -9,12 +9,42 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // MARK: Properties
+    
+    
+    
+    // MARK: IBOutlets
+    @IBOutlet weak var cityNameLabel: UILabel!
+    @IBOutlet weak var cityTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func getDataTapped(_ sender: Any) {
+        // guard let city = cityTextField.text else { return }
+        // cityNameLabel.text = city
+        guard let url = URL(string: "https://api.sunrise-sunset.org/json?lat=36.7201600&lng=-4.4203400") else { return }
+        
+        let session = URLSession.shared
+        session.dataTask(with: url) { (data, response, error) in
+            if let response = response {
+                print(response)
+            }
+            
+            guard let data = data else { return }
+            print(data)
+            
+            do {
+                let json = try JSONSerialization.jsonObject(with: data, options: [])
+                print(json)
+            } catch {
+                print(error)
+            }
+        }.resume()
+    }
+    
 
 }
 
